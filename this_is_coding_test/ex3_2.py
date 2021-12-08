533343409398f931f8d7f4b8b70eb768bee709fc
Ji-yong219 <wldydslapjyy@naver.com>
2021-12-09 01:33:00 +0900

    create ex 3.2

---
+++ this_is_coding_test/ex3_2.py
@@ -0,0 +1,17 @@
+def main():
+    result = 0
+    # n, m, k = [int(i) for i in input().split(" ")]
+    n, m, k = map(int, input().split())
+    data = [int(i) for i in input().split(" ")]
+    data.sort()
+
+    first = data[n-1]
+    second = data[n-2]
+
+    for i in range(m):
+        if (i+1) % k == 0:
+            result += second
+        else:
+            result += first
+
+    return result
