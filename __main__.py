6cb1e537357caa54fa2c2a241e29a415d494b55c
Ji-yong219 <76960714+Ji-yong219@users.noreply.github.com>
2021-12-29 04:09:35 +0900

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
-from time import time
+from datetime import datetime
 from this_is_coding_test.ex3_2 import main
 
 if __name__ == "__main__":
-    start = time()
+    start = datetime.now()
 
     print(main())
 
-    print(f"progress time : {time() - start}s")
+    print(f"progress time : {datetime.now() - start}s")
