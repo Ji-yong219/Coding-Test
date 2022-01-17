7779007a30544dac8282a78cb66a70c1d185f377
Ji-yong219 <wldydslapjyy@naver.com>
2022-01-18 08:34:29 +0900

    split

--- baekjoon/samsung_sw/p13460-2.py
+++ baekjoon/samsung_sw/p13460-2.py
@@ -1,5 +1,12 @@
 from collections import deque
 
+def printBoard(data):
+    for row in data:
+        for c in row:
+            print(f"{c:3}", end="")
+        print("\n")
+    print()
+
 def isPossible(map, R, B, dx, dy):
     x1, y1 = R
     x2, y2 = B
@@ -7,12 +14,21 @@ def isPossible(map, R, B, dx, dy):
     nx1, ny1 = x1 + dx, y1 + dy
     nx2, ny2 = x2 + dx, y2 + dy
 
+    # print(f"({dx}, {dy})\t{nx1}, {ny1}\t{nx2}, {ny2}", end="\t->\t")
+        
     if map[ny1][nx1] in [".", "O"] or map[ny2][nx2] in [".", "O"]:
+        # print("true")
         return True
 
+    # print("false")
     return False
 
-def main(N, M, map):
+def main(r, C, map):
+    global N
+    global M
+
+    N, M = r, C
+
     dq = deque()
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
@@ -22,16 +38,16 @@ def main(N, M, map):
 
     visited = set()
 
+    # printBoard(map)
+
     R, B, O, new_R, new_B = [None for _ in range(5)]
 
+    # finding R & B
     for i in range(N):
         if R and B and O:
             break
         
         for j in range(M):
-            if R and B and O:
-                break
-
             if map[i][j] == "R":
                 R = (j, i)
 
@@ -44,11 +60,24 @@ def main(N, M, map):
     visited.add((R, B))
 
     for i in range(4):
+        # data = isPossible(map, R, B, O, visited, dx[i], dy[i], last)
+        # map2 = [_[:] for _ in map]
+        # data = incline(map2, dx[i], dy[i], R, B, O)
+        # if data and data[:2] not in visited:
         if isPossible(map, R, B, dx[i], dy[i]):
             dq.append( (dx[i], dy[i], R, B, 1) )
 
+    print(dq)
+    printBoard(map)
+    print("="*50)
+
     while dq:
+        print(f"\n\nbefore deque :")
+        for dd in dq:
+            print(dd)
+        # last = x, y
         x, y, R, B, cnt = dq.popleft()
+        print(f"\npop\nxy : ({x}, {y})\tR : {R}, B : {B}")
 
         if new_R:
             map[new_R[1]][new_R[0]] = "."
@@ -59,12 +88,19 @@ def main(N, M, map):
 
             map[O[1]][O[0]] = "O"
 
+        print("\n\nbefore")
+        printBoard(map)
+
         data = incline(map, N, M, x, y, R, B, O)
+        print(f"data : {data}")
 
         new_R, new_B = data[:2]
 
+        print(data[:2], visited)
         if (new_R, new_B) in visited:
             continue
+
+
 
         map[R[1]][R[0]] = "."
         map[B[1]][B[0]] = "."
@@ -76,15 +112,31 @@ def main(N, M, map):
 
         R, B = new_R, new_B
 
+        print("\nafter")
+        print(R, B, new_R, new_B)
+        printBoard(map)
+        print("\n")
+
         if new_R == O and new_R != new_B:
             result.append(cnt)
 
         for i in range(4):
-            if (dx[i], dy[i], R, B) not in dq:
-                dq.append( (dx[i], dy[i], R, B, cnt+1) )
+            print()
+            print(dx[i], dy[i], (x,y))
+
+            if data and data[:2] not in visited:
+                if (dx[i], dy[i], R, B) not in dq:
+                    dq.append( (dx[i], dy[i], R, B, cnt+1) )
+
 
         visited.add( data[:2] )
+        print(f"\nafter deque :")
+        for dd in dq:
+            print(dd)
+        print("\n\n")
 
+
+    print(f"result : {result}")
     result = min(result) if result!=[] else -1
     result = -1 if result > 10 else result
 
@@ -120,6 +172,8 @@ def incline(map, N, M, dx, dy, R, B, O):
         iter2 = range(M-1, -1, -1)
         h = True
         drc = "→"
+
+    print(drc, "before", R, B, end="\t")
 
     if v:
         iter1, iter2 = iter2, iter1
@@ -173,6 +227,12 @@ def incline(map, N, M, dx, dy, R, B, O):
 
                 goal = next_goal
 
+            # printBoard(map)
+                
+        # print("\n\n")
+        # break
+
+
     map[R[1]][R[0]] = "."
     map[B[1]][B[0]] = "."
 
@@ -181,8 +241,10 @@ def incline(map, N, M, dx, dy, R, B, O):
 
     map[O[1]][O[0]] = "O"
 
+    print("after", R, B)
     return R, B, O
 
-N, M = map(int, input().split(" "))
-data = [list(input()) for _ in range(N)]
-print(main(N, M, data))
+if __name__ == "__main__":
+    N, M = map(int, input().split(" "))
+    data = [list(input()) for _ in range(N)]
+    print( main(N, M, data) )
