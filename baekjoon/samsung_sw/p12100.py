768deba7e9dd2758c6339fa89d49d90935d0dc4e
Ji-yong219 <wldydslapjyy@naver.com>
2022-01-31 22:38:30 +0900

    init 2048

--- baekjoon/samsung_sw/p12100.py
+++ baekjoon/samsung_sw/p12100.py
@@ -5,6 +5,8 @@ def main(N, map):
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
 
+    return "hi"
+
     # for i in range(4):
     #     dx[i], dy[i]
 
@@ -15,9 +17,11 @@ def main(N, map):
 
     while dq:
         R1, B1, cnt = dq.popleft()
+        break
 
     return -1 if result > 10 else result
 
-N, M = map(int, input().split(" "))
-data = [list(input()) for _ in range(N)]
-print(main(N, M, data))
+# N, M = map(int, input().split(" "))
+# data = [list(input()) for _ in range(N)]
+
+# print(main(N, M, data))
