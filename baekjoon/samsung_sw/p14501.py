a7fd3183ec3ab7a5059ceb8c8ba66fe31373b08d
Ji-yong219 <wldydslapjyy@naver.com>
2022-02-12 08:18:16 +0900

    Create p14501.py

---
+++ baekjoon/samsung_sw/p14501.py
@@ -0,0 +1,10 @@
+def main(N, plan):
+    result = 0
+
+    return result
+
+if __name__ == "__main__":
+    N = int(input())
+    plan = [ list(map(int, input().split(" "))) for _ in range(N)]
+
+    print( main(N, plan) )
