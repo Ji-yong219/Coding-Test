30d9b957ae838c50bcced9efda9024f08614452d
Ji-yong219 <wldydslapjyy@naver.com>
2022-03-04 13:16:05 +0900

    Update p14501.py

--- baekjoon/samsung_sw/p14501.py
+++ baekjoon/samsung_sw/p14501.py
@@ -8,6 +8,6 @@ def main(plan):
 
 if __name__ == "__main__":
     N = int(input())
-    plan = [ list(map(int, input().split(" "))) for _ in range(N)]
+    plan = [ list(map(int, input().split(" "))) for _ in range(N) ]
 
     print( main(plan) )
