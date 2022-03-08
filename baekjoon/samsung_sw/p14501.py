18841b56d4a90adf08cd9104deefc66315883be7
Jiyong Park <wldydslapjyy@naver.com>
2022-03-08 22:42:04 +0900

    Update p14501.py

--- baekjoon/samsung_sw/p14501.py
+++ baekjoon/samsung_sw/p14501.py
@@ -1,13 +1,24 @@
-def main(plan):
+def main(N, plan):
     result = 0
 
-    for day, (T, P) in enumerate(plan, 1):
-        print(day, T, P)
+    t_day = 1
+    for n_day, (T, P) in enumerate(plan, 1):
+        pay = 0
+        t_day = n_day + T
 
+        while t_day <= N:
+            temp = t_day + plan[t_day-1][0]
+            if temp <= N:
+                t_day = temp
+                pay += plan[t_day-1][1]
+            else:
+                break
+
+        result = max(result, pay)
     return result
 
 if __name__ == "__main__":
     N = int(input())
     plan = [ list(map(int, input().split(" "))) for _ in range(N) ]
 
-    print( main(plan) )
+    print( main(N, plan) )
