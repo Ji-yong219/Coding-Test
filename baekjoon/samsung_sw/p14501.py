a8f8d42c4d3453a3809ce2f6348aa9542ef7f3cd
Ji-yong219 <wldydslapjyy@naver.com>
2022-03-09 23:17:28 +0900

    Update p14501.py

--- baekjoon/samsung_sw/p14501.py
+++ baekjoon/samsung_sw/p14501.py
@@ -4,24 +4,34 @@ def main(N, plan):
     result = 0
     visited = []
 
-    t_day = 1
     for n_day, (T, P) in enumerate(plan, 1):
         if n_day + T > N+1:
             break
         dq = deque([n_day-1])
-        pay = P
-        t_day = n_day + T
 
-        # while dq:
-        while t_day <= N:
-            temp = t_day + plan[t_day-1][0]
-            if temp > N+1:
-                break
-            else:
-                pay += plan[t_day-1][1]
-                t_day = temp
+        while dq:
+            i = dq[-1] + plan[dq[-1]][0]
 
-        result = max(result, pay)
+            while i < N:
+                if i + plan[i][0] > N:
+                    i += 1
+                    continue
+
+                temp = tuple(dq)
+
+                if temp+(i,) not in visited:
+                    dq.append(i)
+                    i += plan[i][0]
+                    continue
+                i += 1
+
+            visited.append(tuple(dq))
+
+            result = max(result, sum([plan[ii][1] for ii in dq]))
+
+            i = dq.pop()
+
+
     return result
 
 if __name__ == "__main__":
