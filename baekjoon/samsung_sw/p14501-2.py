f96b63d06e022042041e9e2b13ad004da0fd1e68
Ji-yong219 <wldydslapjyy@naver.com>
2022-04-09 20:45:58 +0900

    Update p14501-2.py

--- baekjoon/samsung_sw/p14501-2.py
+++ baekjoon/samsung_sw/p14501-2.py
@@ -1,12 +1,16 @@
 def main(N, plan):
     result = 0
     
-    cost = []
+    cost = [0] * (N+1)
 
-    for i in range(N):
-        for j in range(i):
-            print(i, j)
+    for i in range(N-1, -1, -1):
+        d = i + plan[i][0]
+        if d < N:
+            cost[i] = max(cost[i+1], cost[d] + plan[i][1])
+        else:
+            cost[i] = cost[i+1]
 
+    result = max(cost)
     return result
 
 if __name__ == "__main__":
