02df9e4e9378d021b07ff4d2c8a0c05f01495cf4
Ji-yong219 <wldydslapjyy@naver.com>
2022-04-14 21:54:31 +0900

    Update p14888.py

--- baekjoon/samsung_sw/p14888.py
+++ baekjoon/samsung_sw/p14888.py
@@ -1,10 +1,9 @@
-def main(N, M, data, y, x, d):
+def main(N, M, data):
     result = 0
     return result
 
 if __name__ == "__main__":
     N, M = tuple(map(int, input().split(" ")))
-    r, c, d = tuple(map(int, input().split(" ")))
     data = [ list(map(int, input().split(" "))) for _ in range(N) ]
 
-    print( main(N, M, data, r, c, d) )
+    print( main(N, M, data) )
