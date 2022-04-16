4cc13786280a495924efd2d906b6c22b046ca43c
Ji-yong219 <wldydslapjyy@naver.com>
2022-04-16 16:43:58 +0900

    Update p14888.py

--- baekjoon/samsung_sw/p14888.py
+++ baekjoon/samsung_sw/p14888.py
@@ -1,7 +1,34 @@
-def main(N, data):
-    # result = 0
-    a = permutations([1,2,3,], 2)
-    print(a)
+def main(N, AN, ON):
+    result = 0
+
+    oo = [1] * ON[0] + [2] * ON[1] + [3] * ON[2] + [4] * ON[3]
+    operators = permutations(oo, N-1)
+    
+    mx = None
+    mn = None
+    for operator in operators:
+        temp = AN[0]
+        for a, o in zip(AN[1:], operator):
+            if o == 1:
+                temp += a
+            elif o == 2:
+                temp -= a
+            elif o == 3:
+                temp *= a
+            elif o == 4:
+                temp //= a
+        
+        if mx is None:
+            mx, mn = temp, temp
+
+        elif mx < temp:
+            mx = temp
+        elif mn > temp:
+            mn = temp
+
+    result = f"{mx}\n{mn}"
+
+    return result
 
 def permutations(arr, n):
     result = []
@@ -16,6 +43,7 @@ def permutations(arr, n):
 
 if __name__ == "__main__":
     N = int( input() )
-    data = [ list(map(int, input().split(" "))) for _ in range(N) ]
+    AN = list(map(int, input().split(" ")))
+    ON = list(map(int, input().split(" ")))
 
-    print( main(N, data) )
+    print( main(N, AN, ON) )
