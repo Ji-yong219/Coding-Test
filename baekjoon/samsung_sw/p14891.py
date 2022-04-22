62e05817071129e5bdd225e4d300b793254e5f75
Ji-yong219 <wldydslapjyy@naver.com>
2022-04-23 06:48:12 +0900

    Create p14891.py

---
+++ baekjoon/samsung_sw/p14891.py
@@ -0,0 +1,11 @@
+def main(N, L, S):
+    result = 0
+
+
+    return result
+
+if __name__ == "__main__":
+    N, L = list(map(int, input().split(" ")))
+    S = [ list(map(int, input().split(" "))) for _ in range(N)]
+
+    print( main(N, L, S) )
