fdac14db5c4703e44d7f820e4818d82a772cd198
Ji-yong219 <wldydslapjyy@naver.com>
2022-04-24 06:15:13 +0900

    create p23291

--- baekjoon/samsung_sw/p23291.py
+++ baekjoon/samsung_sw/p23291.py
@@ -1,4 +1,4 @@
-def main(N, M, data):
+def main(N, K, data):
     result = 0
 
     D = [
@@ -15,7 +15,7 @@ def main(N, M, data):
     return result
 
 if __name__ == "__main__":
-    N, M = tuple(map(int, input().split(" ")))
+    N, K = tuple(map(int, input().split(" ")))
     data = [list(map(int, input().split(" "))) for _ in range(N)]
 
-    print( main(N, M, data) )
+    print( main(N, K, data) )
