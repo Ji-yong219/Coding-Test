be561caaa0b462e4e515ce89679629d471e0101e
Ji-yong219 <wldydslapjyy@naver.com>
2022-04-24 04:46:13 +0900

    Update p14891.py

--- baekjoon/samsung_sw/p14891.py
+++ baekjoon/samsung_sw/p14891.py
@@ -1,36 +1,45 @@
-def main(gears, K, turns, indexs):
+def main(gears, K, turns):
     result = 0
+    indexs = [[6, 2], [6, 2], [6, 2], [6, 2]]
 
     for g, d in turns:
         axis = [i[:] for i in indexs]
         
-        if d == 1:
-            for i in range(2):
-                indexs[g-1][i] -= 1
+        ld = d
 
-        else:
-            for i in range(2):
-                indexs[g-1][i] += 1
-
+        # 왼쪽 기어들 회전
         for idx in range(g-2, -1, -1):
-            d = -d
+            ld = -ld
             if gears[idx][ axis[idx][1] %8 ] == gears[idx+1][ axis[idx+1][0] %8 ]:
                 break
             else:
-                if d == 1:
+                if ld == 1:
                     for i in range(2):
                         indexs[idx][i] -= 1
                 else:
                     for i in range(2):
                         indexs[idx][i] += 1
                         
+        # 현재 기어 회전
+        # 시계
+        if d == 1:
+            for i in range(2):
+                indexs[g-1][i] -= 1
+
+        # 반시계
+        else:
+            for i in range(2):
+                indexs[g-1][i] += 1
+                        
+        rd = d
+        # 오른쪽 기어들 회전
         for idx in range(g, 4):
-            d = -d
+            rd = -rd
             # 극이 같을 때
             if gears[idx-1][ axis[idx-1][1] %8 ] == gears[idx][ axis[idx][0] %8 ]:
                 break
             else:
-                if d == 1:
+                if rd == 1:
                     for i in range(2):
                         indexs[idx][i] -= 1
                 else:
@@ -46,6 +55,5 @@ if __name__ == "__main__":
     gears = [list(map(int, list(input()))) for _ in range(4)]
     K = int(input())
     turns = [tuple(map(int, input().split(" "))) for _ in range(K)]
-    indexs = [[6, 2], [6, 2], [6, 2], [6, 2]]
 
-    print( main(gears, K, turns, indexs) )
+    print( main(gears, K, turns) )
