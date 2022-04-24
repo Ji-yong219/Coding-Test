07b3894f389b62c0a8066837f1f70333456c9b1d
Ji-yong219 <wldydslapjyy@naver.com>
2022-04-25 06:05:41 +0900

    Update p23291.py

--- baekjoon/samsung_sw/p23291.py
+++ baekjoon/samsung_sw/p23291.py
@@ -1,5 +1,46 @@
-def check(data, K):
-    return max(data) - min(data) <= K
+def step3(grid, N):
+    arr = []
+    for x in range(N):
+        for y in range(N-1, 0, -1):
+            a = grid[y][x]
+            b = grid[y][x-1]
+            c = grid[y-1][x]
+
+            if a == 0:
+                continue
+
+            if b != 0:
+                d = abs(a-b)//5
+                if d > 0:
+                    if a > b:
+                        arr.append(((y, x), (y, x-1), d))
+                    else:
+                        arr.append(((y, x-1), (y, x), d))
+
+            if c != 0:
+                d = abs(a-c)//5
+                if d > 0:
+                    if a > c:
+                        arr.append(((y, x), (y-1, x), d))
+                    else:
+                        arr.append(((y-1, x), (y, x), d))
+
+    for a, b, d in arr:
+        grid[a[0]][a[1]] -= d
+        grid[b[0]][b[1]] += d
+
+def step4(grid, N):
+    data = []
+    for x in range(N):
+        for y in range(N-1, -1, -1):
+            v = grid[y][x]
+            if v == 0:
+                continue
+            data.append(v)
+
+    grid = [[0] * N for _ in range(N-1)]
+    grid.append(data.copy())
+    return grid, data
 
 def main(N, K, data):
     result = 0
@@ -14,69 +55,78 @@ def main(N, K, data):
         # Exit Condition
         if mx - mn <= K:
             break
-        else:
-            result += 1
+
+        result += 1
 
         # Step 1 Start
         while True:
             try:
-                idx = data.index(mn)
-                data[idx] = mn+1
+                idx = grid[-1].index(mn)
+                grid[-1][idx] = mn+1
             except:
                break
         # Step 1 End
-
 
         # Step 2 Start
         y = N-1
         x = 0
         grid[y][x], grid[y-1][x+1] = 0, grid[y][x]
+
         while True:
-            try:
-                xs = 0
-                xe = 0
-                ys = -1
+            xs = -1
+            xe = -1
+            ys = -1
 
-                # Find X index higher than 2F
-                for i in range(N):
-                    if xs == 0 and grid[N-2][i] != 0:
-                        xs = i
-                    if xs != 0 and grid[N-2][i] == 0:
-                        xe = i
-                        break
+            # Find X index higher than 2F
+            for i in range(N):
+                if xs == -1 and grid[N-2][i] != 0:
+                    xs = i
+                    xe = i + 1
+                elif xs != -1 and grid[N-2][i] != 0:
+                    xe = i + 1
 
-                # Find Y index higher than 2F
-                for i in range(N):
-                    if grid[i][xs] != 0:
-                        ys = i
-                        break
-
-                if xe+N-ys+1 >= N:
+            # Find Y index higher than 2F
+            for i in range(N):
+                if grid[i][xs] != 0:
+                    ys = i
                     break
 
-                for xb, ya in zip(range(xs, xe), range(N-(xe-xs)-1, N-1)):
-                    for yb, xa in zip(range(N-1, ys-1, -1), range(xe, xe+N-ys+2)):
-                        grid[ya][xa], grid[yb][xb] = grid[yb][xb], 0
-                        
-
-            except:
+            if xe - ys > 0:
                 break
 
-        break
+            for xb, ya in zip(range(xs, xe), range(N-(xe-xs)-1, N-1)):
+                for yb, xa in zip(range(N-1, ys-1, -1), range(xe, xe+N-ys+2)):
+                    grid[ya][xa], grid[yb][xb] = grid[yb][xb], 0
+                    
         # Step 2 End
 
+        step3(grid, N)
+        grid, data = step4(grid, N)
 
-        # Step 3 Start
+        # Step 5 Start
+        c = N//2
+        d = 1
+        xs = c-1
+        xe = -1
+        ys = N-2
+        ye = N-1
+        for _ in range(2):
+            for yb, ya in zip(range(N-1, N-d-1, -1), range(ys, ye)):
+                for xb, xa in zip(range(xs, xe, -1), range(N-c, N)):
+                    grid[ya][xa], grid[yb][xb] = grid[yb][xb], 0
 
-        # Step 3 End
+            ye = ys
+            ys -= d*2
+            d *= 2
 
+            xe = xs
+            c //= 2
+            xs += c
 
-        # Step 4 Start
+        # Step 5 End
 
-        # Step 4 End
-    
-    for r in grid:
-        print(r)
+        step3(grid, N)
+        grid, data = step4(grid, N)
 
     return result
 
@@ -85,4 +135,3 @@ if __name__ == "__main__":
     data = list(map(int, input().split(" ")))
 
     print( main(N, K, data) )
-
