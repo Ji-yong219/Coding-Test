993d89d0007c3478d52f0a8de07a3656ea56f285
Jiyong Park <wldydslapjyy@naver.com>
2022-05-12 03:49:16 +0900

    Update coin.py

--- study/coin.py
+++ study/coin.py
@@ -9,36 +9,41 @@ coins = [
 R = [1440, 11, 46, 74]
 
 def cal(r, cs, dp):
-    result = 0
-
-    a = dp[r-1]
-    a = 0 if a == -1 else a
-
-    x = 1
-    cal(r - (x) , cs, dp)
-
-
-    for e in  [r-c-1 for c in cs]:
-        result += dp[e] if dp[e]>0 else 0
-
-    dp[r-1] = result
-
-    temp = min(cal(r-1, cs, dp), cal(r-2, cs, dp), cal(r-3, cs, dp)) + 1
-    print(f"temp : {temp}")
+    if r < 1:
+        return -1
     
-    return temp
-    # return min(cal(r-1, cs, dp), cal(r-2, cs, dp), cal(r-3, cs, dp)) + 1
+    if dp[r] == -1:
+        return -1
+
+    elif dp[r] > 0:
+        return dp[r]
+
+    else:
+        tp = []
+        for c in cs:
+            t = cal(r-c, cs, dp)
+            if t == -1:
+                continue
+            tp.append(t)
+
+        if tp == [] or min(tp) < 1:
+            return -1
+
+        else:
+            dp[r] = min(tp)+1
+            return min(tp) + 1
 
 
 for cs, r in zip(coins, R):
-    dp = [0] * r
-    dp[0] = 0
-    dp[1] = -1
-    dp[2] = 1
+    dp = [0] * (r+1)
+    mn = min(cs)
+    for i in range(0, mn+1):
+        dp[i] = -1
 
-    for i in range(3, r):
-        cal(i, cs, dp)
+    for c in cs:
+        dp[c] = 1
 
-    cal(r, cs, dp)
+    dp[mn] = 1
 
-print(dp)
+    t = cal(r, cs, dp)
+    print(f"동전 {cs} 중 {r}를 만드는 최소 개수 : {t}개")
