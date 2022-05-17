36cc1b5b5e0d9dcde36e17aff110817c7f5ac380
Ji-yong219 <wldydslapjyy@naver.com>
2022-05-17 23:28:59 +0900

    Update p15683.py

--- baekjoon/samsung_sw/p15683.py
+++ baekjoon/samsung_sw/p15683.py
@@ -9,11 +9,15 @@ def main(N, M, data):
         ((0, 1), (1, 0), (0, -1), (-1, 0))
     ]
 
-    for r in data:
-        for c in r:
-            print(c, end=" ")
+    for y in range(N):
+        for x in range(M):
+            c = data[y][x]
             if 0 < c < 6:
-                print(D[c-1])
+                for i in range(4):
+                    ny = y + D[c-1][i][0]
+                    nx = x + D[c-1][i][1]
+
+                    print(data[ny][nx])
 
         print()
 
