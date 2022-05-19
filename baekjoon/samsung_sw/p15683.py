51950c33d2049d8991ef3d2be2124d08a86fc645
Ji-yong219 <wldydslapjyy@naver.com>
2022-05-19 22:06:25 +0900

    Update p15683.py

--- baekjoon/samsung_sw/p15683.py
+++ baekjoon/samsung_sw/p15683.py
@@ -1,25 +1,55 @@
 def main(N, M, data):
     result = 0
 
+    DC = (4, 2, 4, 4, 1)
+
     D = [
         ((0, 0), (1, 0), (0, 0), (0, 0)),
+
         ((0, 0), (1, 0), (0, 0), (-1, 0)),
+
         ((0, 1), (1, 0), (0, 0), (0, 0)),
+
         ((0, 1), (1, 0), (0, 0), (-1, 0)),
+
         ((0, 1), (1, 0), (0, -1), (-1, 0))
     ]
+
+
+
+
+    for r in data:
+        print(r)
+    print("")
+
+    cams = []
 
     for y in range(N):
         for x in range(M):
             c = data[y][x]
 
-            if 0 < c < 6:
-                for i in range(4):
-                    ny = y + D[c-1][i][0]
-                    nx = x + D[c-1][i][1]
+            if c != "#" and 0 < c < 6:
+                cams.append((y, x, data[y][x]-1))
 
-                    print(data[ny][nx])
-        print()
+    CS = [[j for j in range(DC[i[2]])] for i in cams]
+    print(f"CS : {CS}")
+
+
+                # for i in range(4):
+                #     ny = y + D[c-1][i][0]
+                #     nx = x + D[c-1][i][1]
+
+                    # while 0 <= ny < N and 0 <= nx < M and data[ny][nx] != 6:
+                    #     if data[ny][nx] == 0:
+                    #         data[ny][nx] = "#"
+                    #
+                    #     ny = ny + D[c-1][i][0]
+                    #     nx = nx + D[c-1][i][1]
+
+    for r in data:
+        print(r)
+
+    print(f"cams : {cams}")
 
     return result
 
