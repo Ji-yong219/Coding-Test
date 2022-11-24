354f7ffc3a0839b757364ff4e93a5656ebb76b83
Ji-yong219 <wldydslapjyy@naver.com>
2022-11-24 21:19:11 +0900

    feat: "C++ Algorithm 2.2.10"

--- CPP/algorithm/2.2.10.cpp
+++ CPP/algorithm/2.2.10.cpp
@@ -3,7 +3,26 @@
 using namespace std;
 
 int main(void) {
-    
+    int N = 0;
+    int M = 0;
+    int A = 0;
+    int B = 0;
+    int i = 0;
+    int j = 0;
+
+    scanf("%d %d", &N, &M);
+
+    int num_arr[N][M] = {0, };
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            cin >> num_arr[i][j];
+        }
+    }
+
+    scanf("%d %d", &A, &B);
+
+    printf("%d", num_arr[A][B]);
 
     return 0;
 }
