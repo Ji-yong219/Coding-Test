0c7045557013f5f77eb6f1048e7e65736c1935a5
Ji-yong219 <wldydslapjyy@naver.com>
2022-11-26 02:04:48 +0900

    refactor: "C++ Algorithm 2.2.11"

--- CPP/algorithm/2.2.11.cpp
+++ CPP/algorithm/2.2.11.cpp
@@ -8,17 +8,18 @@ int main(void) {
     int j = 0;
 
     int prev_num1 = 1;
-    int prev_num2 = 0;
+    int prev_num2 = 1;
 
     cin >> N;
 
     for (i = 0; i < N; i++) {
-        for (j = 0; j < N - i; j++) {
-            printf("%d ", j + prev_num1 + prev_num2 );
-            prev_num1 = j + prev_num1 + prev_num2;
+        for (j = 0 + i; j < N; j++) {
+            printf("%d ", j + prev_num1 );
+            prev_num1 = j + prev_num1;
         }
-        prev_num1 = prev_num1 - j;
-        prev_num2 += prev_num1 + i;
+
+        prev_num2 += i + 1;
+        prev_num1 = prev_num2;
         printf("\n");
     }
 
